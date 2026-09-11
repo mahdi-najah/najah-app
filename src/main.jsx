3d9bc98264c865ca760b382fdca3e07b8518f208
mahdi-najah <mahdizk44@gmail.com>
2026-09-11 12:20:33 +0100

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NajahApp from '../najah-app.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NajahApp />
+  </React.StrictMode>
+)
